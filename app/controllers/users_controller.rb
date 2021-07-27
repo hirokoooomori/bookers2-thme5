faafa0a50dc9a40fef631fc915647c5793583b28
hirokoooomori <hirouta0408@gmail.com>
2021-07-27 02:43:53 +0000

    [Update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,14 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
